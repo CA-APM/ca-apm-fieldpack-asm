9fc29073041ba5ea93c7f6643c086edbdfce0fa0
grogu01 <Guenter.Grossberger@ca.com>
2015-08-06 16:47:45 -0700

    moved config fiel to config directory

--- asm-monitor/src/main/java/com/ca/apm/swat/epaplugins/utils/AsmProperties.java
+++ asm-monitor/src/main/java/com/ca/apm/swat/epaplugins/utils/AsmProperties.java
@@ -23,7 +23,7 @@ public interface AsmProperties {
     public static final String ASM_PRODUCT_NAME         = "App Synthetic Monitor";
     public static final String ASM_PRODUCT_NAME_SHORT   = "ASM";
 
-    public static final String PROPERTY_FILE_NAME       = "AppSyntheticMonitor.properties";
+    public static final String PROPERTY_FILE_NAME       = "config/AppSyntheticMonitor.properties";
     public static final String PASSWORD_URL             =
             "https://dashboard.cloudmonitor.ca.com/en/change_passwd.php";
 
